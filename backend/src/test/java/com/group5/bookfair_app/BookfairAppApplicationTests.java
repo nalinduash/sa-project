5ae48a12f1fb632f0061148a5e4bd1b3fa2d9bdb
NalinduAsh <nalindugra@gmail.com>
2026-02-12 22:06:04 +0530

    Initial commit

---
+++ backend/src/test/java/com/group5/bookfair_app/BookfairAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.bookfair_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookfairAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
