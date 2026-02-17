f51af5093d9cd3c1f29773ebea863f0e4f84cc3f
NalinduAsh <nalindugra@gmail.com>
2026-02-17 10:03:15 +0530

    cleanup 2

--- backend/src/main/java/com/bookfair/service/AuthService.java
+++ backend/src/main/java/com/bookfair/service/AuthService.java
@@ -14,6 +14,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
